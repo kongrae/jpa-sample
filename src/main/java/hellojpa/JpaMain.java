8d7d62169c313f2199ff9e7d33eb39dd3f145aa8
kongrae <ghdfo918@gmail.com>
2022-09-04 21:12:50 +0900

    영속성 컨텍스트2

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -25,12 +25,33 @@ public class JpaMain {
 //            Member findMember = em.find(Member.class, 1L);
 //            findMember.setName("HelloJPA");
 
-//            JPQL CASE22
-            List<Member> result = em.createQuery("SELECT m from Member as m", Member.class)
-                    .getResultList();
-            for (Member member : result){
-                System.out.println("member = " + member.getName());
-            }
+//            JPQL CASE
+//            List<Member> result = em.createQuery("SELECT m from Member as m", Member.class)
+//                    .getResultList();
+//            for (Member member : result){
+//                System.out.println("member = " + member.getName());
+//            }
+            // 비영속
+//            Member member = new Member();
+//            member.setId(101L);
+//            member.setName("HellpJPA");
+
+            // 영속
+//            System.out.println("=== BEFORE ===");
+//            em.persist(member);
+//            System.out.println("=== AFTER ===");
+//
+//            Member findMember = em.find(Member.class, 101L);
+//            System.out.println("findMember.id => " + findMember.getId());
+//            System.out.println("findMember.name => " + findMember.getName());
+//            Member findMember1 = em.find(Member.class, 101L);
+//            Member findMember2 = em.find(Member.class, 101L);
+//
+//            System.out.println("result = "  + (findMember1 == findMember2));
+            Member member = em.find(Member.class, 150L);
+            member.setName("changeA");
+
+            System.out.println("====================");
 
             tx.commit();
         } catch (Exception e) {
